43b38d3f3b6b2d79666bcff8688c7d2d406cca26
RubensGS <rubensgoncalvessouza@gmail.com>
2020-06-05 00:36:55 -0300

    Concluindo BackEnd da aplicação

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import path from 'path';
+import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    response.json(['Rubens', 'Nathalia', 'Felipe']);
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
